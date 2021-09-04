92aabbd8d9ebf692347a7e03bb647891cb339192
rorschach-27 <rorschach_zhao@outlook.com>
2021-09-04 14:27:48 +0800

    Update _priority_queue.h

--- 06_Heapsort/_priority_queue.h
+++ 06_Heapsort/_priority_queue.h
@@ -9,11 +9,11 @@
 
 class _max_priority_queue: public _heap{
 public:
-    int _heap_maximum();//return maximum of heap.
+    int _heap_maximum();//return maximum of heap.p163
 
-    int _heap_extract_max();//
+    int _heap_extract_max();//p163
 
-    void _max_heap_insertion(int key);//insert a new element.
+    void _max_heap_insertion(int key);//insert a new element.p164
 
     void _heap_delete(int i);//delete i-th element of a heap.6.5-8
 
